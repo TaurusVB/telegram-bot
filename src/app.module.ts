2f081598d32846b8521dff2d29271a9274132025
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-12-24 12:45:30 +0200

    UPD: telegram bot message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,40 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TelegrafModule } from 'nestjs-telegraf';
-
-import { Update, Ctx, Start, Help, On, Hears } from 'nestjs-telegraf';
 import { ChatgptModule } from './chatgpt/chatgpt.module';
+import { TelegramModule } from './telegram/telegram.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    TelegrafModule.forRootAsync({
-      useFactory: () => ({
-        token: '6584897423:AAH1ho5c3J9e4nT_EqeCJYB6UDRoWfD85XY',
-      }),
-    }),
-    ChatgptModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ChatgptModule, TelegramModule],
 })
-@Update()
-export class AppUpdate {
-  @Start()
-  async start(@Ctx() ctx) {
-    await ctx.reply('Welcome');
-  }
-
-  @Help()
-  async help(@Ctx() ctx) {
-    await ctx.reply('Send me a sticker');
-  }
-
-  @On('sticker')
-  async on(@Ctx() ctx) {
-    await ctx.reply('👍');
-  }
-
-  @Hears('hi')
-  async hears(@Ctx() ctx) {
-    await ctx.reply('Hey there');
-  }
-}
-
 export class AppModule {}
--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Ctx, Message, On, Start, Update } from 'nestjs-telegraf';
+import { Scenes, Telegraf } from 'telegraf';
 
-@Injectable()
-export class TelegramService {}
+type Context = Scenes.SceneContext;
+
+@Update()
+export class TelegramService extends Telegraf<Context> {
+  @Start()
+  onStart(@Ctx() ctx: Context) {
+    ctx.replyWithHTML(`<b>Hello, ${ctx.from.username}</b>
+It's chat bot with ChatGPT
+Write the questions and i answer them!
+    `);
+  }
+
+  @On('text')
+  onMessage(@Message('text') message: string, @Ctx() ctx: Context) {
+    ctx.replyWithHTML(`<i>${message}</i>`);
+  }
+}
