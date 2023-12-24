2053c5174a283a03b40845ad2e700ad05095bd1b
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-12-24 12:05:45 +0200

    init telegram and chatgpt modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,40 @@
 import { Module } from '@nestjs/common';
+import { TelegrafModule } from 'nestjs-telegraf';
+
+import { Update, Ctx, Start, Help, On, Hears } from 'nestjs-telegraf';
+import { ChatgptModule } from './chatgpt/chatgpt.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TelegrafModule.forRootAsync({
+      useFactory: () => ({
+        token: '6584897423:AAH1ho5c3J9e4nT_EqeCJYB6UDRoWfD85XY',
+      }),
+    }),
+    ChatgptModule,
+  ],
 })
+@Update()
+export class AppUpdate {
+  @Start()
+  async start(@Ctx() ctx) {
+    await ctx.reply('Welcome');
+  }
+
+  @Help()
+  async help(@Ctx() ctx) {
+    await ctx.reply('Send me a sticker');
+  }
+
+  @On('sticker')
+  async on(@Ctx() ctx) {
+    await ctx.reply('👍');
+  }
+
+  @Hears('hi')
+  async hears(@Ctx() ctx) {
+    await ctx.reply('Hey there');
+  }
+}
+
 export class AppModule {}
